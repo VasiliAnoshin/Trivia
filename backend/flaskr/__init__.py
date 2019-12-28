b251ac6ac9bb1850bab9c982d5eab92d40eae44d
BenjaminFranklin <anoshin45@gmail.com>
2019-12-28 14:33:34 +0200

    remove all TODO sections

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import random
+import sys
 
 from models import setup_db, Question, Category
 
@@ -54,34 +55,17 @@ def create_app(test_config=None):
         if len(sorted_questions)==0:
           return None
         return  (random.choice(sorted_questions)).format()
-  '''
-  @TODO: 
-  Create an endpoint to handle GET requests 
-  for all available categories.
-  '''
+
   @app.route('/categories', methods=['GET'])
   def get_categories():
     categories = Category.query.all()
     formatted_ctg = [ctg.type for ctg in categories]
-    print(formatted_ctg)
     if(len(formatted_ctg)==0):
       abort(404)
     return jsonify({ 
       'categories': formatted_ctg,
     })
 
-  '''
-  @TODO: 
-  Create an endpoint to handle GET requests for questions, 
-  including pagination (every 10 questions). 
-  This endpoint should return a list of questions, 
-  number of total questions, current category, categories. 
-
-  TEST: At this point, when you start the application
-  you should see questions and categories generated,
-  ten questions per page and pagination at the bottom of the screen for three pages.
-  Clicking on the page numbers should update the questions. 
-  '''
   @app.route('/questions', methods=['GET'])
   def questions():
     questions = Question.query.all()
@@ -96,12 +80,6 @@ def create_app(test_config=None):
       'categories': get_formatted_categories(),
     })
         
-  '''
-  @TODO: 
-  Create an endpoint to DELETE question using a question ID. 
-  TEST: When you click the trash icon next to a question, the question will be removed.
-  This removal will persist in the database and when you refresh the page. 
-  '''
   @app.route('/questions/<int:id>', methods=['DELETE'])
   def delete_question(id):
     try:
@@ -115,26 +93,6 @@ def create_app(test_config=None):
       'success':True
     })
 
-  '''
-  @TODO: 
-  Create a POST endpoint to get questions based on a search term. 
-  It should return any questions for whom the search term 
-  is a substring of the question. 
-
-  TEST: Search by any phrase. The questions list will update to include 
-  only question that include that string within their question. 
-  Try using the word "title" to start. 
-  '''
-  '''
-  @TODO: 
-  Create an endpoint to POST a new question, 
-  which will require the question and answer text, 
-  category, and difficulty score.
-
-  TEST: When you submit a question on the "Add" tab, 
-  the form will clear and the question will appear at the end of the last page
-  of the questions list in the "List" tab.  
-  '''
   @app.route('/questions', methods=['POST'])
   def post_new_question():
     try:
@@ -159,13 +117,6 @@ def create_app(test_config=None):
     except:
       abort(422)
 
-  '''
-  @TODO: 
-  Create a GET endpoint to get questions based on category.
-  TEST: In the "List" tab / main screen, clicking on one of the 
-  categories in the left column will cause only questions of that 
-  category to be shown. 
-  '''
   @app.route('/categories/<int:id>/questions', methods=['GET'])
   def get_question_by_category(id):
      try:
@@ -181,32 +132,18 @@ def create_app(test_config=None):
       'categories': get_formatted_categories(),   
     })
 
-  '''
-  @TODO: 
-  Create a POST endpoint to get questions to play the quiz. 
-  This endpoint should take category and previous question parameters 
-  and return a random questions within the given category, 
-  if provided, and that is not one of the previous questions. 
-
-  TEST: In the "Play" tab, after a user selects "All" or a category,
-  one question at a time is displayed, the user is allowed to answer
-  and shown whether they were correct or not. 
-  '''
   @app.route('/quizzes', methods=['POST'])
   def play_quizz():
     try:
         data = request.get_json()
         rand_quest = get_queez_question(data)
     except:
+       print('This is error output', file=sys.stderr)
        abort(422)
     return jsonify({
         'question': rand_quest,
      })     
-  '''
-  @TODO: 
-  Create error handlers for all expected errors 
-  including 404 and 422. 
-  '''
+
   @app.errorhandler(404)
   def not_found(error):
     return jsonify({
